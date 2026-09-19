741b778d0a2b2cd0c16d3b2381d69f71ddd267c4
zwallpe <wallpe@calpoly.edu>
2026-09-18 21:27:20 -0700

    Core: Flappy Fighters settings

---
+++ config.js
@@ -0,0 +1,18 @@
+window.GAME_CONFIG = {
+  title: 'Flappy Fighters',
+  fix: 'custom',
+  canvasWidth: 360,
+  canvasHeight: 640,
+  gravity: 1400,
+  flapStrength: 420,
+  birdSize: 34,
+  pipeWidth: 64,
+  pipeGap: 150,
+  pipeSpacing: 260,
+  pipeSpeed: 150,
+  groundHeight: 80,
+  modes: {
+    easy: { pipeGap: 190, pipeSpeed: 110 },
+    normal: { pipeGap: 150, pipeSpeed: 150 }
+  }
+};
